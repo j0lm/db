a023e1ce7fbc8f6494b3769501ec60da371f03c1
Jose <jose0lmedojr@gmail.com>
2024-04-02 03:15:54 -0500

    starter

---
+++ db.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import os
+import argparse
+
+def parser_init():
+    parser = argparse.ArgumentParser(prog='db', description='keep track of files')
+
+def parse_args():
+    parser.add_argument('-n','--new')
+
+def main():
+
---
+++ file.py
@@ -0,0 +1,3 @@
+class file:
+path = ""
+    tags = []
